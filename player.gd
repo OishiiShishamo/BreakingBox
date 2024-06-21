f80c615fc9be2c0b1d014cb4a0c97b755a04e880
Shishamo <shishamodesu@outlook.jp>
2024-06-21 23:32:17 +0900

    init

--- player.gd
+++ player.gd
@@ -1,35 +1,39 @@
 extends Area2D
 
+var window_potision:Vector2
+var player_speed = 60
+var speed = 5
+var screen_size = get_viewport_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Vector2 default_window_potision = get_window().position
-	Vector2 window_potision = Vector2(0, 0)
+	window_potision = get_window().position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_window().position = Vector2(default_window_potision.x + window_potision.x, default_window_potision.x + window_potision.x)
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x -= 1
-		window_potision.x += 1
+		#window_potision.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x += 1
-		window_potision.x -= 1
+		#window_potision.x -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1
-		window_potision.y += 1
+		#window_potision.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y += 1
-		window_potision.y -= 1
+		#window_potision.y -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	position += velocity * delta * player_speed
+	window_potision += Vector2(-velocity.x, -velocity.y)
+	get_window().position = Vector2i(window_potision)
+	
 	pass
