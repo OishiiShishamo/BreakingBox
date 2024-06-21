461681e3b79f36716571495506812a0823f70055
Shishamo <shishamodesu@outlook.jp>
2024-06-21 22:37:43 +0900

    init

--- player.gd
+++ player.gd
@@ -11,4 +11,25 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	DisplayServer.window_set_ime_position (Vector2i(default_window_potision.x + window_potision.x, default_window_potision.x + window_potision.x))
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x -= 1
+		window_potision.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += 1
+		window_potision.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+		window_potision.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+		window_potision.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
 	pass
